5abbbde23e09cbbf633801745b449313d2736503
Joshua Basche <joshbasche@live.com>
2021-08-21 16:35:08 -0700

    create startNewRound function

--- BullsEyeiOS15/ViewController.swift
+++ BullsEyeiOS15/ViewController.swift
@@ -9,27 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  //IBOutlets
+  @IBOutlet weak var slider: UISlider!
+  
+  
   //Variables
   var currentValue: Int = 0
+  var targetValue: Int = 0
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    startNewRound()
   }
-
+  
+  func startNewRound() {
+    targetValue = Int.random(in: 1...100)
+    currentValue = 50
+    slider.value = Float(currentValue)
+  }
+  
   @IBAction func showAlert() {
+    let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
     
-    let alert = UIAlertController(title: "Hello World", message: "This is my first app!", preferredStyle: .alert)
+    let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
     
-    let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+    let action = UIAlertAction(title: "OK", style: .default, handler: nil)
     
     alert.addAction(action)
     present(alert, animated: true, completion: nil)
     
+    startNewRound()
   }
   
   @IBAction func sliderMoved(_ slider: UISlider) {
-    print("The value of the slider is now \(slider.value)")
+    currentValue = lroundf(slider.value)
   }
   
   
