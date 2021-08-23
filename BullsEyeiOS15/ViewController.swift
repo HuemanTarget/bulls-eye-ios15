50e87b0542e6b53cded8d76617bcadcd2b8f2f51
Joshua Basche <joshbasche@live.com>
2021-08-23 10:52:04 -0700

    add transition animation to StartOver button

--- BullsEyeiOS15/ViewController.swift
+++ BullsEyeiOS15/ViewController.swift
@@ -47,6 +47,12 @@ class ViewController: UIViewController {
     roundValue = 0
     scoreValue = 0
     startNewRound()
+    
+    let transition = CATransition()
+    transition.type = CATransitionType.fade
+    transition.duration = 1
+    transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+    view.layer.add(transition, forKey: nil)
   }
   
   @IBAction func showAlert() {
