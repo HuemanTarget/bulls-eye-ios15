1ee7acf29cfae14f9fbf041a5d15aed05b97ce02
Joshua Basche <joshbasche@live.com>
2021-08-23 10:29:11 -0700

    create AboutViewController

---
+++ BullsEyeiOS15/AboutViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.swift
+//  AboutViewController
+//
+//  Created by Joshua Basche on 8/23/21.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+  @IBAction func close() {
+    dismiss(animated: true, completion: nil)
+  }
+}
